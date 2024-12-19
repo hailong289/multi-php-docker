009f40e5564c24697ecded00f0094f7c2c8e1deb
Hải Long <longdh2.dev@gmail.com>
2024-12-19 17:22:05 +0700

    feat: update docker-compose move command to sh file

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -2,7 +2,7 @@
 # Thay thế biến môi trường trong tất cả các file .template
 for template in /etc/nginx/conf.d/*.conf.template; do
     [ -e "$template" ] || break  # Thoát nếu không có tệp nào
-    envsubst '${SERVER_NAME1} ${SERVER_NAME2} ${SERVER_NAME3}' \
+    envsubst '${SERVER_NAME1} ${SERVER_NAME2} ${SERVER_NAME3} ${SERVER_NAME4}' \
     < "$template" > "/etc/nginx/conf.d/$(basename "${template%.template}")"
 done
 
