e3e76d3d986669ccb44232fc80ddef0cb6f1b48b
Hải Long <longdh2.dev@gmail.com>
2025-04-03 16:37:40 +0700

    feat: auto create template nginx

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-for template in /etc/nginx/templates/*.conf.template; do
+for template in /etc/nginx/templates/*.template; do
     [ -e "$template" ] || break  # Thoát nếu không có tệp nào
     # Sao chép tệp từ /etc/nginx/templates/ vào /etc/nginx/conf.d/ mà không giữ phần mở rộng .template
     cp "$template" "/etc/nginx/conf.d/$(basename "${template%.template}.conf")"
