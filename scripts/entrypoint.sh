137e2216769482b21298288fb668eddd2dd7af6a
Hải Long <longdh2.dev@gmail.com>
2025-04-03 15:33:46 +0700

    feat: auto create template nginx

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,18 +1,50 @@
 #!/bin/sh
-if [ ! -f "/var/scripts/entrypoint.sh" ]; then
-  echo "ERROR: /var/scripts/entrypoint.sh not found"
-  exit 1
+# Tạo file config nginx từ template cho từng server_name trong env.json
+JSON_FILE="env.json"
+
+if [ ! -f "$JSON_FILE" ]; then
+    echo "JSON file not found: $JSON_FILE"
+    exit 1
 fi
-variables=$(env | grep '^SERVER_NAME' | cut -d= -f1 | sed 's/^/${/;s/$/}/' | tr '\n' ' ');
-# Thay thế biến môi trường trong tất cả các file .template
-for template in /etc/nginx/templates/*.conf.template; do
-    [ -e "$template" ] || break  # Thoát nếu không có tệp nào
-    envsubst "$variables" \
-    < "$template" > "/etc/nginx/conf.d/$(basename "${template%.template}")"
+
+if ! command -v jq &> /dev/null; then
+    echo "jq command not found. Please install jq."
+    exit 1
+fi
+
+keys=$(jq -r 'keys_unsorted | .[] | select(test("SERVER_NAME[0-9]*"))' "$JSON_FILE")
+
+# Định nghĩa đường dẫn
+TEMPLATE_FILE="nginx/examples/server_example.txt"
+OUTPUT_DIR="nginx/templates"
+
+# Kiểm tra file template tồn tại
+if [ ! -f "$TEMPLATE_FILE" ]; then
+    echo "Template file not found: $TEMPLATE_FILE" >&2
+    exit 1
+fi
+
+# Tạo thư mục đầu ra nếu chưa tồn tại
+mkdir -p "$OUTPUT_DIR"
+# Lặp qua từng server
+for key in $keys; do
+    # Trích xuất NAME và PATH từ JSON bằng jq
+    DOCKER_APP_NAME=$(jq -r --arg key "$key" '.[$key].APP_NAME' "$JSON_FILE")
+    DOCKER_HOSTNAME=$(jq -r --arg key "$key" '.[$key].DOMAIN_NAME' "$JSON_FILE")
+    DOCKER_SOURCE_PATH=$(jq -r --arg key "$key" '.[$key].SERVER_PATH' "$JSON_FILE")
+
+    if [ -n "$DOCKER_APP_NAME" ] && [ -n "$DOCKER_HOSTNAME" ] && [ -n "$DOCKER_SOURCE_PATH" ]; then
+        OUTPUT_FILE="$OUTPUT_DIR/${DOCKER_APP_NAME}.template"
+
+        # Thay thế cả SERVER_NAME và SERVER_PATH trong template
+        sed -e "s|\${SERVER_NAME}|${DOCKER_HOSTNAME}|g" \
+            -e "s|\${SERVER_PATH}|${DOCKER_SOURCE_PATH}|g" \
+            "$TEMPLATE_FILE" > "$OUTPUT_FILE"
+
+        echo "Tạo config nginx thành công: $OUTPUT_FILE"
+    else
+        echo "Biến môi trường bị thiếu: $key"
+    fi
 done
 
-# Thay thế biến môi trường trong nginx.conf
-#envsubst '${SERVER_NAME1} ${SERVER_NAME2} ${SERVER_NAME3}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
-
-# Khởi động Nginx
 exec nginx -g 'daemon off;'
