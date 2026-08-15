f32d594e1423b7b25b4479b8799e17b2246c851e
Hải Long <longdh2.dev@gmail.com>
2026-08-15 11:35:26 +0700

    feat(server_manage): Update package-lock.json with new dependencies

--- server/manager/public/assets/xml-CcLGTatT.js
+++ server/manager/public/assets/xml-CcLGTatT.js
@@ -1,4 +1,4 @@
-import{m as r}from"./editor.main-Bc8JJZrw.js";import"./index-BT-FiWVF.js";/*!-----------------------------------------------------------------------------
+import{m as r}from"./editor.main-DbK96jgQ.js";import"./index-C7X6vZHo.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.52.2(404545bded1df6ffa41ea0af4e8ddb219018c6c1)
  * Released under the MIT license
