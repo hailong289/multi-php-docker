c35c7b6b73d518f223fcec98d9a520721ae45001
Hai Long <longdh2.dev@gmail.com>
2026-08-29 12:53:23 +0700

    fix(add_host_macos): detach hosts applet and serialize hosts writes

--- server/manager/public/assets/cssMode-Ylz3LHzl.js
+++ server/manager/public/assets/cssMode-Ylz3LHzl.js
@@ -1,4 +1,4 @@
-import{m as et}from"./editor.main-COyStX4T.js";import"./index-DtgjyH0X.js";/*!-----------------------------------------------------------------------------
+import{m as et}from"./editor.main-DtqFEq6Z.js";import"./index-uwL-s55W.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.52.2(404545bded1df6ffa41ea0af4e8ddb219018c6c1)
  * Released under the MIT license
